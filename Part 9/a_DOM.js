4aa1f76952bf8bf09289fae1cbfea8e207430f5c
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2023-12-20 09:35:10 +0000

    commit

--- Part 9/a_DOM.js
+++ Part 9/a_DOM.js
@@ -17,7 +17,7 @@ console.dir(document.all[9])
 // we can change it's innerText
 document.all[9].innerText="Peter Parker"
 
-   // Getting Element By Id
+   // * Getting Element By Id *
 // getElementById ,returns element as an object or null
 document.getElementById("mainImg"); 
 let a=document.getElementById("mainImg")  //by this we can access Id and check and Modify Various things
@@ -30,7 +30,7 @@ a.src="assets/creation_2.jpeg"  //we can chnage src
 document.getElementById("desription")
 console.dir(document.getElementById("desription"))
 
-     // Getting elements by className
+     // * Getting elements by className *
 // getElementsByClassName ,returns elements as an HTML collections or empty
 // Not an array but similar to an array, we can iterate, find length but can't push and pop
 // #{{only get id have element rest all have elements}}
@@ -44,13 +44,95 @@ for(let i=0; i<=smallimages.length; i++){
     console.dir(smallimages[i]);   // Or "smallimages[i].src" for all sources of images
 }
 
-   // Getting Elements by TagName
+   // * Getting Elements by TagName *
 // getElementByTagName ,Returns element as an HTML collection or empty collection
 document.getElementsByTagName("p")  // all paragraph will display in serial number
 document.getElementsByTagName("p")[1].innerText="Hellow How are You"
 document.getElementsByTagName("span") //as span is not available, we will get "HTML collections" as an output.
 
-    // Query Selector
+    // * Query Selector *
 // Benifits, we don't need to remember different selector Names
+console.dir(document.querySelector("h1"))  //heading
+console.dir(document.querySelector("#description"))  //Id
+console.dir(document.querySelector(".images"))  //class
+
+// Demerit, It select only 1st object of that class, not whole
+// like only 1st heading, 1st paragraph, 1st class
+
+console.dir(document.querySelector("div a"));  //will select only 1st anchor tag inside div
+console.dir(document.querySelectorAll("div a"))  //will select all anchor tags, will get Node list
+
+
+  // ****** DOM Manipulation ,using properties and methods ********
+let para=document.querySelector("p")
+console.dir(para) // it will display alot of properties of it
+
+// innerText, Shows the visible text contained in a node(one of the property)
+para.innerText;
+
+// innerContent, Shows full text(like display: none etc)
+para.innerContent;
+
+// innerHTML, Shows full text + Markup
+para.innerHTML;
+
+// for Manipulation
+para.innerHTML="Hey, I am <b>Akansha kumari</b>"
+para.innerHTML=`<u>${para.innerHTML}</u>`
+
+
+   // *** Manipulating Attributes ***
+// attributes like id, class, style etc(example, attributes of image tag= src, alt)(attributes of a tag= href etc)
+Object.getAttribute(attr) //to get
+Object.setAttribute(attr) //to set
+
+let img=document.querySelector('img') //1st image will display (as tag name is img)
+img  //src will print
+img.getAttribute("id")  //[~ mainImg] //by this we will get id
+img.setAttribute("id","SpiderMan Image")  // we can change id
+img.setAttribute("src","assets/creation_3.jpeg")  // can change src
+img.getAttribute("class")  //[~ NULL]
+img.setAttribute("class","Imagez")  //we can set the class also
+
+   // ***** Manipulating Style ******
+console.dir(img)  // will get alot properties of this object
+img.style  // style, one of it's property
+
+let heading=document.querySelector("h1")
+console.dir(heading)
+heading.style
+heading.style.color="yellow"  //by this we can set the color
+heading.style.backgroundColor="red"   //in js Camel Case works
+
+let links=document.querySelectorAll('.box a')
+for(let i=0;i<links.length;i++){
+     links[i].style.color="red"     
+} 
+  // OR
+for(link of links){
+   links.style.color="red"; 
+}
+// But by this, we can only set inline styling, not proper styling in css page!!
+// Like we can't find styles in "heading.style", which is set in css Page!!
+
+  //  **** Using Class List ****
+let image=document.querySelector("img")
+console.dir(image)   // as like "style","classList" is also one of it's property
+image.classList   // we will get all classes if have
+image.classList.add("Helow")    // we can add multiple classes like this
+image.classList.remove("Helow")   
+
+// why to choose ".classList.add()" over ".setAttribute("class","ClassName")"
+// in .setAttribute we can acess only single class at a time, but in classList.add we can add and acess all multiple clases at time
+
+image.classList.contains("backgroundColor")  //~[False]
+// through contains we can check backgroundColor or any more properties is set in this particular class
+
+image.classList.toggle("Helow")
+// Toggle means, if True will become False vise versa
+// by this if Helow class is already present, it will remove it ~[output = FALSE]
+// else if not present, will add it ~[output = TRUE]
+
+
 
 
---
+++ Part 9/b_Navigation.js
@@ -0,0 +1,11 @@
+// In Navigation, we can find or move from one to another
+let h4 = document.querySelector("h4")
+console.dir(h4)  //we can get all properties from here
+h4.parentElement   //one of it's property
+
+let box=document.querySelector(".box")
+console.dir(box)
+box.children
+box.childElementCount  //we will get child count
+
+// Prvious siblings
