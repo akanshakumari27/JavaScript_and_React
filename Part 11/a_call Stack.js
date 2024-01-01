921c3b3252f3b4e5ecbf3c762737988bbcefc4a6
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2024-01-01 15:53:19 +0000

    Call Stack and Breakpoints

--- Part 11/a_call Stack.js
+++ Part 11/a_call Stack.js
@@ -8,7 +8,7 @@ function two(){
 function three(){
     return two() + one()  
 }
-three()
+three() // calling it in Global Scope
 //If we call three(), it will first call two(), two() will call one(), now final call is to one(){last in} will complete it's work and return back to two(){first out}
 //Now viseVersa two() will also finish it's work and return to three()
 //And finally three() get it's two(), now it will again directly call to one() and find sum and give Result {first in last out}
@@ -22,5 +22,5 @@ three()
 // 1) Sources -> top -> app.js 
 // 2) First alot any breakpoint(can be multiple breakpoints), where code will stop during/after Run! Refresh
 // 3) press "arrowdown"(Step into next function call), we can see all multiple steps/every single execution in Stack
-// In Scope also we will get outputs after every execution
-// This is how we can check every single steps of stack and can debug it!!
+// 4) In "Scope" also we will get outputs after every execution
+// 5) This is how we can check every single steps of stack and can debug it!!
