47c4b02c6e03c1c1281be35c5b799d7f5e2dac45
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2024-01-20 04:04:12 +0000

    Final TouchUp

--- MiniProject/c_Simon Game.js
+++ MiniProject/c_Simon Game.js
@@ -5,6 +5,9 @@ let btns=["red","yellow","blue","green"]
 let started=false;
 let level=0;
 
+let h3=document.querySelector("h3")
+let body= document.querySelector("body")
+
 document.addEventListener("keypress",function(){
     if(started==false){
         console.log("game started")
@@ -13,13 +16,15 @@ document.addEventListener("keypress",function(){
     }
 })
 function levelUp(){
+    userSeq = []
     level++;
-    let h3=document.querySelector("h3")
     h3.innerText=`Level ${level}`
 
-    let randIdx = Math.floor(Math.random()*3)
+    let randIdx = Math.floor(Math.random()*4)
     let randColors = btns[randIdx]
     let randBtns = document.querySelector(`.${randColors}`)
+    gameSeq.push(randColors)
+    console.log(gameSeq)
     flashGame(randBtns)
 }
 function flashGame(btn){
@@ -38,12 +43,47 @@ for(btn of allBtns){   //this btn is in it's own functional scope
     btn.addEventListener("click",btnPress)
 }
 function btnPress(){
+    console.log(this)  //point out that particular button element
     let btn=this
     flashUser(btn)
+
+    userColor = btn.getAttribute("id")
+    console.log(userColor)
+    userSeq.push(userColor)
+
+    checkAns(userSeq.length-1)
 }
 function flashUser(btn){
     btn.classList.add("flashUser")  //this btn is in it's own functional scope
     setTimeout(function(){
         btn.classList.remove("flashUser")
+    },250)
+}
+function checkAns(idx){   
+    if(userSeq[idx]===gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,1000)
+        }
+    }else{
+        h3.innerHTML = `Game Over! Your Score is <b>${level}</b> <br>Press any key to Start again!` //innerHtml coz html tags won't apply in innerText
+        reset()
+        flashBackground()
+        //OR
+        // document.querySelector("body").style.backgroundColor="red"
+        // setTimeout(function(){
+        //     document.querySelector("body").style.backgroundColor = "rgb(113, 205, 208)"
+        // },250)
+    }
+}
+function reset(){
+    started = false
+    level = 0
+    gameSeq = []
+    userSeq = []
+}
+function flashBackground(){
+    body.classList.add("flashBody")
+    setTimeout( function(){
+        body.classList.remove("flashBody")
     },250)
 }
