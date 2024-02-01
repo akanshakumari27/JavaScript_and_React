43b105dd0fcefdf21a04cdc7694e59004319cffa
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2024-02-01 12:50:17 +0000

    React

--- React/Part 2/a_React props.jsx
+++ React/Part 2/a_React props.jsx
@@ -44,7 +44,7 @@ function ProductTab(){
 function Product(props){
     console.log(props)  //Object
     return(
-        <div className = "Product">
+        <div className = "Product"> {/* For styling "Product class" in css file */}
         <h4>{props.title}</h4>
         <p>Paragraph</p>
         </div>
@@ -72,3 +72,124 @@ function Product({title, price="4000"}){ //we can assign default value also, if 
     )
 }
 
+// *** Writing Props in form of Array and Object(Key-value) pair ***
+// ProductTab.jsx
+function ProductTab(){
+    let options1 = ["hey, ","hello, ","how are you"] //bydefault there will be no space, so need to provide extra space
+    let options2 = {veggie:"tomato", fruit:"apple", tree:"banana tree"} //Object only works when there only 1 Product to return
+    return( 
+        //all props must inside in {}
+        <>
+        <Product 
+        title="Phone" 
+        price={30000} 
+        feature1 = {options1} 
+        feature2 = {options2}
+        feature3 = {["my ","name ","is Akansha"]}  /* writing Array directly */
+        feature4 = {{phone:"micromax", pc:"hp", desktop:"dell"}}/>  {/* writing Object directly */}
+        </>
+    )
+}
+// Product.jsx
+function Product({title, price, feature1, feature2, feature3, feature4}){ 
+    return(
+        <div className = "Product"> 
+        <h4>{title}</h4>  {/*all Props in curly braces*/}
+        <p>{feature1}</p>
+        <p>{feature2.veggie}</p>
+        <p>{feature3}</p>
+        <p>{feature4.pc}</p> {/*Need to abstract particular key from oject otherwise it won't work*/}
+        </div>
+    )
+}
+
+// *** Rendering Array, extracting or printing all Array elements indvidually ****
+function ProductTab(){
+    let arrayElement = [<li>"Hello!"</li>,<li>"How"</li>,<li>"are"</li>,<li>"you"</li>]
+    return( 
+        <>
+        <Product 
+        title="Phone" 
+        price={30000} 
+        elements = {arrayElement} /> 
+        </>
+    )
+}
+function Product({title, price,elements}){ 
+    return(
+        <div className = "Product"> 
+        <h4>{title}</h4>
+        <p>{price}</p>
+        <p>{elements}</p>
+        </div>
+    )
+}
+ 
+// Second Method
+function ProductTab(){
+    let arrayElement = ["Hello!","How","are","you"]
+    return( 
+        <>
+        <Product 
+        title="Phone" 
+        price={30000} 
+        elements = {arrayElement} /> 
+        </>
+    )
+}
+function Product({title, price, elements}){ 
+    return(
+        <div className = "Product"> 
+        <h4>{title}</h4>
+        <p>{price}</p>
+        <p>{
+         elements.map((element)=>(<li>{element}</li>)) //Map is used to iterate over an array and return new array()=>()
+        }</p>
+        </div>
+    )
+}
+
+
+// *** Conditionals: Adding Elements on basis of Some Conditions ***
+function ProductTab(){
+    return( 
+        <>
+        <Product title="Phone" price={30000} /> 
+        <Product title="EarPod" price={15000} /> 
+        <Product title="Tab" price={50000} /> 
+        </>
+    )
+}
+function Product({title, price}){ 
+    if(price>40000){
+        return(
+            <div className = "Product"> 
+            <h4>{title}</h4>
+            <p>{price}</p>
+            <p>Discount: 5%</p>
+            </div>
+        )
+    }else{
+        return(
+            <div className = "Product"> 
+            <h4>{title}</h4>
+            <p>{price}</p>
+            </div>
+        )
+    }
+}  
+// Better and Optimised method with Dynamic Component of Styling
+function Product({title, price}){
+    let getStyle = {backgroundColor : price>30000 ? "pink":""}
+    return(
+        <div className = "Product" style={getStyle}> {/*Writing "style" in jsx*/}
+        <h4>{title}</h4>
+        <p>{price}</p>
+        {/* <p>{price>40000 ? "Discount: 5%":""}</p> ,but this creates empty paragraph tag for false statement*/}
+        {price>40000 ? <p>Discount: 5%</p>:null}
+        {price<20000 ? <p>No Acesseries</p>: <a href='./'>Extra Acceseries</a>}
+        {price>40000 && <p>Discount:5%</p>} 
+        {/* && operator, if stament False nothing will execute as false&& = false*/}
+        </div>
+    )
+}
