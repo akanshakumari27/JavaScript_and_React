e4615855e9399f09309fad32e977f52495c37178
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2025-08-12 19:24:53 +0530

    Update c_activity.js

--- Part 10/c_activity.js
+++ Part 10/c_activity.js
@@ -1,15 +1,18 @@
-let btn = document.querySelector("button")
-let h3=document.querySelector("h3")
-let div=document.querySelector("div")
-btn.addEventListener("click",function(){
-    h3.innerText=getNum();
-    div.style.backgroundColor=getNum();
+// Genrate Random Colour Task:
+let button = document.querySelector('button');
+let h3 = document.querySelector('h3');
+let yes = document.querySelector('.box')
+
+button.addEventListener('click',function(){
+    let randcolor = genRandColor();
+    h3.innerText = `My colour is ${randcolor}`;
+    yes.style.backgroundColor = `rgb${randcolor}`;
 })
-function getNum(){
-    let red=Math.floor(Math.random()*255)
-    let green=Math.floor(Math.random()*255)
-    let blue=Math.floor(Math.random()*255)
-    let op=Math.floor(Math.random()*100)
-    let num=`rgb(${red},${green},${blue},.${op})`
-    return num;
+
+function genRandColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    let color = `(${red},${green},${blue})`;
+    return color;
 }
