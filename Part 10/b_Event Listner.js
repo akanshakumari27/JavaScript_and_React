884194d325d90f972a500934b35b2fbc50fc4ee6
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2025-08-12 17:10:06 +0530

    Update b_Event Listner.js

--- Part 10/b_Event Listner.js
+++ Part 10/b_Event Listner.js
@@ -1,12 +1,13 @@
-// But as in previous methods, we can't call 2 functions together for a single click
-// therfor we can't perform 2 activities together for single click
+// But as in previous methods, we can't call 2 or more functions for a single click, by using onclick
+// therfor we can't perform 2 or more activities together for single click
 
 // Event listners listens what we want to perform by clicking, dragging, keybordkey etc and perform for us
+// MDN addeventlistner
 
 let buttons = document.querySelectorAll("button")
 for(btn of buttons){
-    // btn.onclick=sayHello   //this won't work
-    // btn.onclick=Gupta
+    // btn.onclick = sayHello  // this will not work
+    // btn.onclick = Gupta     // By onclick, only last declared function will work
     btn.addEventListener("click",sayHello)
     btn.addEventListener("click",Gupta)
     btn.addEventListener("dblclick",function(){
