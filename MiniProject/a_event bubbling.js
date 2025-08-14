23087f03767a7bd44444005f51256084ef3ece22
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2025-08-14 13:38:07 +0530

    Update a_event bubbling.js

--- MiniProject/a_event bubbling.js
+++ MiniProject/a_event bubbling.js
@@ -2,12 +2,12 @@ let div=document.querySelector("div")
 let ul=document.querySelector("ul")
 let li=document.querySelector("li")
 
-// If we click ul or li, it will call it's parents also, example(clicked div, it will call("div, ul, li"))known as "Event Bubbling"
+// If we click ul or li, it will call it's parents also, example(click li, it will call("div, ul, li") all), this is known as "Event Bubbling"
 div.addEventListener("click",function(){
     console.log("div is clicked")
 }) 
 ul.addEventListener("click",function(event){
-    event.stopPropagation() //to prevent Event Bubbling we use this
+    event.stopPropagation() //to prevent Event Bubbling we can use use this
     console.log("ul is clicked")
 })
 li.addEventListener("click",function(event){
