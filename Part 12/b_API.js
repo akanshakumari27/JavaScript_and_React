768e1634f441c627ec4487507786ad7d5257d334
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2025-08-19 21:21:33 +0530

    Update b_API.js

--- Part 12/b_API.js
+++ Part 12/b_API.js
@@ -1,8 +1,16 @@
 // Application Programming Interface
-// Generally 2 softwares interact through API's
-// But when any API interact through "https protocol", means interact on bases of internet, Web API
-// User - API - server
+// Generally 2 softwares interact through API's, it’s a way for different software programs to communicate with each other.
+// But when any API interact through "https protocol", means interact on bases of internet are known as Web API
+// APIs can help update a webpage in real-time without reloading it (eg: Like button in Instagram, after clicking like, page didn't reload again)
+
+// Example 1: Zomato haven't build it's own Map, it's using Google's Map API (Here zomato efforts and time saved)
+// Example 2: While Signup in few Websites, they directly asked, signup with Google or Microsoft (because these 2 have vast stored data about user)
+// So those new Websites saved time and energy, to again go verify new users, because verified users are already exist in Google and Microsoft
+
+// But as those websites like Zomato etc, are allowed to gather only required information of user, not all info!! so for this security, they use API Keys
+
 // user sends the request to API, therfor API usually a url or link(end point) and API returns data in form of JSON
+// Therefore then we use that data in JSON form only
 
 // Some Random APIs
 // https://catfact.ninja/fact (sends random cat facts)(in json)
@@ -14,20 +22,20 @@
 // JSON "Javascript Object Notation"
 // It not only part of js," It's just an Format ", if call data API from any language it will return JSON only
 
-// Earlier data was returned in XML form(very simillar to HTML), but now JSON
-// JSON simillar to key:value pair of object But not Object, like "undefined" is in js object value but not in JSON
+// Earlier data was returned in XML form(very simillar to HTML), but nowdays it returns in JSON format.
+// JSON simillar to key:value pair of object But not Object, like "undefined" is object value in JS but not in JSON
 // to check all JSON value/properties, www.json.org
-// can also check that given json is valid or not, json validator
+// can also check that given json is valid or not; json validator
 
 let file={"fact": "Cats make about 100 different sounds. Dogs make only about 10.","length": 62}
 console.log(file.fact)
 
-// when JSON is in string format, we can use parse to extract it from string
+// In JS, JSON comes in string format, so we need to use parse to extract it from string
 // Acessing data from JSON
 // a) JSON.parse(data) -- to parse a string data into js object
 // b) JSON.stringify(data) -- to parse js object data into json
 
-let json='{"fact": "Cats make about 100 different sounds. Dogs make only about 10.","length": 62}' //actual json
+let json='{"fact": "Cats make about 100 different sounds. Dogs make only about 10.","length": 62}'; //actual json
 let js=JSON.parse(json) 
 console.log(js.fact)
 
@@ -40,14 +48,16 @@ console.log(js.fact)
  JSON.stringify(yay) //get output in json
 
  //  **** Testing API requets ****
+ // Here we will see Tools to test APIs
  // Tools: Hoppscoth(we will work with this, no singup required) and postman(need to download and singup), though both are simillar
  // https://hoppscotch.io/
 
 
- // Ajax (Asynchronus javascript and xml)
- // here the request go from "Js(user) to API" and data return from "API to Js in JSON" {before data used to return in xml so, naming is done according to it before}
+ // Ajax (Asynchronus javascript and XML)
+ // {before data used to return in xml so, naming is done according to it; but now we recive data in form of JSON}
+ // Here the request go from "JS(user) to API" and data return from "API to JS in JSON" (This is done Asynchronusly)
  // for all work done Asynchronusly, Example: if like any post in instagram it will send request to API and increase like internally 
- // without loading whole insta page that's known as asynchronus work
+ // without loading whole insta page that's known as asynchronus work.
 
  // few example of "http verb", when API interact through "http protocol"{https://hoppscotch.io/}
  // "get" when we get an output from API in JSON
@@ -55,7 +65,7 @@ console.log(js.fact)
  // "delete" when we delete anything in JSON
  
 
-//  Status Codes {Status code MDN}
+//  Status Codes (in hopscoth app) {Status code MDN}
 //  Examples :
 //   200 - 0K
 //   404 - Not Found
@@ -64,10 +74,13 @@ console.log(js.fact)
 
 
 // Adding Informations in URL
-// https://www.google.com/search?q=harry+poter (q=harry+poter: Key-value Pair) "Query String" (for space +)
-// ?name=Akansha&age=22 (?name=Akansha(key-value pair) &age=22(and another key-value pair)) "Query String" (for and &)
+// https://www.google.com/search?q=harry+poter [q=harry+poter: Key(q)-value(harry+poter) Pair]
+// ?name=Akansha&age=22 (?name=Akansha(key-value pair) &age=22(and another key-value pair)) "Query String"(&)
 // So whenever we want to pass additional information to url, we use "Query String"
-// https://api.potterdb.com
+// https://api.potterdb.com  (u can normally search in Google: Harry potter API OR/ Twitter API)
+// Scroll down -> APIs Docs -> API -> REST -> Expand to see all avilable APIs
+// https://potterdb.com/  : in additonal add given API and paste Whole link to Hoppscotch site!! 
+
 // key-constant but value-variable
 // if written 2 key-value pairs with "&" and one is invalid, then it will ignore the invalid one and we will recieve output only for valid one!
-// Meta data -- data about data
+// Meta data means; data about data
