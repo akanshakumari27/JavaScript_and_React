3194d41774f8c768007daccacc5f7b6bd2e97246
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2025-08-20 06:39:36 +0530

    Update c_First Api request.js

--- Part 12/c_First Api request.js
+++ Part 12/c_First Api request.js
@@ -1,16 +1,18 @@
 let url = "https://catfact.ninja/fact"
-fetch(url) // inspect - network - last random - response(or js response)
-//paste fetch(url) in inspect returns **"Promise"** thrfor we can apply Promise methods
+fetch(url) // inspect - network - All - fact - preview
+// This is how you can check API Status
+//paste fetch(url) in console window, it will return **"Promise"** thrfore we can apply Promise methods on it
 // API calls asynchronusly
 
 // we can see the ans/output in network response but can't in console, it will display "body:ReadableStream" 
-// therfor to see response in console, we will first "fetch it" and if sucesfully fetched, we will asked to "return it's json"
+// therfore to see response in console, we will first "fetch it" and if sucesfully fetched, we will asked to "return it's json"
 // now to see json data inside, if we recived sucesfully json, then console data for "json file"
 
 fetch(url)
- .then((response)=>{  
+ .then((response)=>{   //the response we recieve from URL
    //  console.log(response.json()) //it will return Promise, therfor we can use Promise methods(then,catch)
-    return response.json()
+    return response.json() //this also returns promise, this helps to read our Data
+    console.log(response.json());
  })
  .then((data)=>{
    console.log("data 1: ",data.fact)
@@ -27,14 +29,31 @@ fetch(url)
  })
 console.log("Hellow") // js won't wait for asynchronus function, it will continously execute(so probably this will execute first)
 
+// ************************************************************************************************************************************* //
 
 // Now fetch by using async and await
 let url2 = "https://catfact.ninja/fact"
 
 async function getFacts(){
+    let res = fetch(url2);
+    console.log(res);      
+}
+
+// getFacts() in console will return undefined because now its async function
+// therefore it will not wait and prints the result, and it might happen that we haven't recieved any result yet from API
+// that's y we will use await, so it will complete asynchronus call and then prints the result
+
+async function getFacts(){
+    let res = await fetch(url2);
+    console.log(res);      
+}
+
+//to print only data from API, we will again use .json
+// this printing two random Facts
+async function getFacts(){
    try{
-    let res = await fetch(url2)  //as "both are asynchronus", as "they return Promise" therfor we will use "await"
-    let data = await res.json()  //as "both are asynchronus", as "they return Promise" therfor we will use "await"
+    let res = await fetch(url2)  
+    let data = await res.json()  
     console.log(data.fact)
 
     let res2 = await fetch(url2)
@@ -46,4 +65,4 @@ async function getFacts(){
     }
     console.log("Yay Few Facts are here!!")  //"try and catch" incase any error, it will not afect further this excution!!
    }
-getFacts()
+getFacts() //in console window
