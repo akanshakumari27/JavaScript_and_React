2457f494f70f92307e993e9d63fc9687bd1b9e1f
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2025-08-24 07:27:20 +0530

    Update d_axios.js

--- Part 12/d_axios.js
+++ Part 12/d_axios.js
@@ -3,6 +3,9 @@
 // Search Axios github -> scroll down -> install (you can install diffrent axios from this)
 // But we will go to CDN section, copy CDN link and paste in HTML Header section (Script Tag)
 
+// Use quotes (' ' or " ") - When you are passing a string literal (fixed text).
+// Do NOT use quotes - When you are passing a variable that holds the URL [axios.get(url)]
+
 /*    -- HTML Page --
     <h1>Getting Random Facts</h1>
     <button>Click Me! to get Facts</button>
@@ -12,8 +15,8 @@
 
 // Now write same code using AXIOS
 // Why to use Axios over Fetch?
-// fetch → You need to manually call .json() 
-// Axios → Response data is already converted to JSON.
+// fetch → You need to manually call and convert into .json() 
+// Axios → Response data is already converted to JSON
 
 let url="https://catfact.ninja/fact"
 let btn = document.querySelector("button")
@@ -24,7 +27,7 @@ btn.addEventListener("click",async ()=>{  //as hav() is an async function, therf
 })
 async function hav(){
   try {
-    let getFact = await axios.get(url) //axios.get() = fetch()
+    let getFact = await axios.get(url) //axios.get(url) returns a response object, not the fact directly. So for data, getFacts.data.
     console.log(getFact.data.fact)
     // return getFact.data.fact [if linking this code with above 
   } catch (error) {
@@ -35,6 +38,32 @@ async function hav(){
 // But why need to replace axios.get() from fetch()
 // because in fetch we got json file, then later to see data we again need to do .json() but in axios we can directly jump to data
 
+// ******* This when you want to Return TWO Facts ********
+let url = 'https://catfact.ninja/fact'
+let btn = document.querySelector('button')
+let p = document.querySelector('p')
+
+btn.addEventListener('click',async ()=>{
+    let ans = await get();
+    p.innerText = `Fact 1: ${ans.fact1}\nFact 2: ${ans.fact2}`;
+    // p.innerText = ans;
+})
+
+async function get(){
+    try{
+        let getFacts = await axios.get(url)
+        let getFacts2 = await axios.get(url)
+
+        return{
+            fact1: getFacts.data.fact,
+            fact2: getFacts2.data.fact
+        }
+    }
+    catch(er){
+        let error = `You Caught Error ${er}`;
+        return error;
+    }
+}
 
 /*
     <h1>Getting Random Dog Images</h1>
@@ -51,13 +80,12 @@ let div=document.querySelector("div")
 console.dir(div)
 btn1.addEventListener("click",async ()=>{  
   let getFunc = await hav();
-  image.setAttribute("src",getFunc.data.message)
-  div.innerHTML = getFunc.data.message
+  image.setAttribute('src', getFunc)
 })
 async function hav(){
   try {
     let getImg = await axios.get(url1)
-    return getImg;
+    return getImg.data.message;;
   } catch (error) {
     let er = console.log("Error: ",error)
     return er;
