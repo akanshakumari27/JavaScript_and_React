a9bb6eaa354dbde63d8e521d59f4cac0a53199f8
Akansha kumari <94097093+akanshakumari27@users.noreply.github.com>
2025-09-06 09:10:58 +0530

    Update a_forms.js

--- React/Part 6/a_forms.js
+++ React/Part 6/a_forms.js
@@ -1,1 +1,15 @@
+import {useState} from 'react'
+export default function Button(){
+    let [name, setName] = useState("Akansha")
 
+    function get(event){
+        console.log(event.target.value)
+        setName(event.target.value)
+    }
+    return(
+        <>
+        <input type="text" placeholder="Enter your Name" value={name} onChange={get} />
+        <button>Submit</button>
+        </>
+    )
+}
